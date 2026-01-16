1402e2f6546e9e3e2ab9366c3d875d783f72c9ef
RoyCuthbert <roycuthbert@hotmail.com>
2026-01-16 23:14:20 +0000

    Added javascript to change how images are swapped with the weather change images function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,5 +6,18 @@ function activateReturnDate(input) {
   input.focus();
 }
 
+function weatherChangeImages(){
+    const hotImages = document.getElementById("image-set-hot-weather");
+    const coldImages = document.getElementById("image-set-cold-weather");
+    const weatherToggle = document.getElementById("weather-toggle")
+    weatherToggle.addEventListener("change", weatherChangeImages)
 
+    if (weatherToggle.checked) {
+        coldImages.style.display = "none";
+        hotImages.style.display = "block";
+    }else{
+        coldImages.style.display = "block";
+        hotImages.style.display = "none";
+    }
 
+}
